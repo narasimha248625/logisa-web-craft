b252eb7cb1d3b9392e9c1672e6725e6ecfdb9e16
varahanarasimha Logisa <varahanarasimhalogisa25@gmail.com>
2025-06-22 17:22:38 +0530

    final done

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: mode === "production" ? "/logisa-web-craft/" : "/",
   server: {
     host: "::",
     port: 8080,
